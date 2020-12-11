03646d74343811654ab66d553a932d3b6a5f4233
Shahid Hussain <38121426+SHAHIDNX@users.noreply.github.com>
2020-12-11 20:03:13 +0530

    Create maxSumSubarray.cpp
    Kadane's algorithm to find Subarray having maximum sum suitable mainly for non negative numbers because if numbers are positive them summation of all numbers will be the maximum subarray sum.
    
    Time Complexity : o(n)
    Space Complexity : o(1)

---
+++ maxSumSubarray.cpp
@@ -0,0 +1,33 @@
+
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+int maxSubArraySum(int arr[], int n)
+{
+    int max_sum = arr[0];
+    int temp = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        temp += arr[i];
+        if (temp > max_sum)
+        {
+            max_sum = temp;
+        }
+        if (temp < 0)
+            temp = 0;
+    }
+    return max_sum;
+}
+
+/*Driver program to test maxSubArraySum*/
+int main()
+{
+    int a[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+    int n = sizeof(a) / sizeof(a[0]);
+    int max_sum = maxSubArraySum(a, n);
+    cout << "MAX SUM is: " << max_sum << endl;
+    return 0;
+}
